d9530709a432f2141fd5f92ef965f7425be8ef01
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-20 13:35:32 +0300

    up

--- Lab Material/Lab 8 Web API using Next/banking-app/app/api/accounts/route.js
+++ Lab Material/Lab 8 Web API using Next/banking-app/app/api/accounts/route.js
@@ -1,3 +1,14 @@
 export async function GET(request) {
     return Response.json({ message: "Welcome to /api/account GET method" }, { status: 200 })
 }
+export async function PUT(request) {
+    return Response.json({ message: "Welcome to /api/account PUT method" }, { status: 200 })
+}
+
+export async function DELETE(request) {
+    return Response.json({ message: "Welcome to /api/account DELETE method" }, { status: 200 })
+}
+
+export async function POST(request) {
+    return Response.json({ message: "Welcome to /api/account POST method" }, { status: 200 })
+}
