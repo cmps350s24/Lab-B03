e054eaa954cb418760ddd4d5f4e9520b4a8dbbd8
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-20 14:27:26 +0300

    update

--- Lab Material/Lab 8 Web API using Next/banking-app/app/api/accounts/route.js
+++ Lab Material/Lab 8 Web API using Next/banking-app/app/api/accounts/route.js
@@ -4,18 +4,14 @@ const accountsRepo = new AccountsRepo()
 
 export async function GET(request) {
     const { searchParams } = new URL(request.url)
+    const type = searchParams.get("type")
 
-    const accounts = await accountsRepo.getAccounts()
-    return Response.json(filteredAccounts, { status: 200 })
-}
-export async function PUT(request) {
-    return Response.json({ message: "Welcome to /api/account PUT method" }, { status: 200 })
-}
-
-export async function DELETE(request) {
-    return Response.json({ message: "Welcome to /api/account DELETE method" }, { status: 200 })
+    const accounts = await accountsRepo.getAccounts(type)
+    return Response.json(accounts, { status: 200 })
 }
 
 export async function POST(request) {
-    return Response.json({ message: "Welcome to /api/account POST method" }, { status: 200 })
+    const account = await request.json()
+    const newAccount = await accountsRepo.addAccount(account)
+    return Response.json(newAccount, { status: 200 })
 }
