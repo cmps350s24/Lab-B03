871ff3f4d0db06c05178128e6dfc31d14273072e
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-20 13:52:52 +0300

    up

--- Lab Material/Lab 8 Web API using Next/banking-app/app/api/accounts/route.js
+++ Lab Material/Lab 8 Web API using Next/banking-app/app/api/accounts/route.js
@@ -1,5 +1,20 @@
+const accounts = [
+    { "acctType": "Saving", "balance": 9999999, "minimumBalance": 1000, "accountNo": "xwTQEw8omvIspjznc52Hn" },
+    { "acctType": "Saving", "balance": 9090, "minimumBalance": 1000, "accountNo": "a05GpsOzAMPJ6OZ4eA_WG" },
+    { "acctType": "Saving", "balance": 9090, "minimumBalance": 1000, "accountNo": "hyCkQZAnt5m1f9ntm-rOU" },
+    { "acctType": "Current", "balance": 1100110011, "monthlyFee": 151515, "accountNo": "4G9isZvsErQpKJAF_7SQB" },
+    { "acctType": "Current", "balance": 1100110011, "monthlyFee": 151515, "accountNo": "1MzIEOsce7MRXzLTsatKi" },
+    { "acctType": "Saving", "balance": "1111", "accountNo": "yqZReSlhItRqR0c51mDEo" },
+    { "acctType": "Current", "balance": "9090", "accountNo": "gYAvkYbE1lDm6FXYSTiuu" }
+]
+// http://localhost:3000/api/accounts?acctType=Saving
+
 export async function GET(request) {
-    return Response.json({ message: "Welcome to /api/account GET method" }, { status: 200 })
+    const { searchParams } = new URL(request.url)
+
+    const acctType = searchParams.get("acctType")
+
+    return Response.json(accounts, { status: 200 })
 }
 export async function PUT(request) {
     return Response.json({ message: "Welcome to /api/account PUT method" }, { status: 200 })
@@ -12,3 +27,5 @@ export async function DELETE(request) {
 export async function POST(request) {
     return Response.json({ message: "Welcome to /api/account POST method" }, { status: 200 })
 }
+
+
