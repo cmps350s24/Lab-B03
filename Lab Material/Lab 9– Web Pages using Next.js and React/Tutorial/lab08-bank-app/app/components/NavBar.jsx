8f2b127784f819e3aa1a291cca558b8d5edf8879
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-03 14:28:05 +0300

    Nav bar with link

--- Lab Material/Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/NavBar.jsx
+++ Lab Material/Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from '@/app/page.module.css'
+import Link from 'next/link'
 
 export default function NavBar() {
     return (
         <nav className={styles.nav}>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/accounts/add">Add Account</a></li>
-                <li><a href="/accounts/transaction">Transaction</a></li>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/accounts/add">Add Account</Link></li>
+                <li><Link href="/accounts/transaction">Transaction</Link></li>
             </ul>
         </nav>
     )
