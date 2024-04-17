5d4375b6c612d7e03630a86ca56f5d6b607905ac
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-17 16:33:39 +0300

    edit up

--- Lab Material/Lab 10– Web Pages using Next.js and React-Forms/Tutorial/lab10-bank-app/app/accounts/Account.jsx
+++ Lab Material/Lab 10– Web Pages using Next.js and React-Forms/Tutorial/lab10-bank-app/app/accounts/Account.jsx
@@ -27,7 +27,6 @@ export default function Account({ account, onDelete }) {
                             {
                                 pathname: '/accounts/upsert',
                                 query: account
-
                             }
                         }>Edit</Link>
                     </i>
--- Lab Material/Lab 10– Web Pages using Next.js and React-Forms/Tutorial/lab10-bank-app/app/accounts/upsert/page.jsx
+++ Lab Material/Lab 10– Web Pages using Next.js and React-Forms/Tutorial/lab10-bank-app/app/accounts/upsert/page.jsx
@@ -2,11 +2,14 @@
 
 import { React, useState } from 'react'
 import styles from '@/app/page.module.css'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 
 export default function page() {
-    const [account, setAccount] = useState({})
+
+    const searchParams = useSearchParams()
+
+    const [account, setAccount] = useState(Object.fromEntries(searchParams) || {})
     const router = useRouter()
 
     function handleChange(e) {
@@ -16,52 +19,71 @@ export default function page() {
     }
     async function handleSubmit(e) {
         e.preventDefault()
-        const url = '/api/accounts'
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(account)
+        if (!account.accountNo) {
+            const url = '/api/accounts'
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(account)
 
-        })
+            })
+        }
+        else {
+            const url = `/api/accounts/${account.accountNo}`
+            const response = await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(account)
+
+            })
+        }
+
         router.push('/')
     }
     return (
         <>
-            <h3 className={styles.title}>Add Account</h3>
+            {!account.accountNo ?
+                <h3 className={styles.title}>Add Account</h3> :
+                <h3 className={styles.title}>Edit Account</h3>
+            }
+
+
             <form id="account-form" className={styles.form} onSubmit={e => handleSubmit(e)}>
                 <label htmlFor="firstname">First Name</label>
-                <input type="text" name="firstname" id="firstname" onChange={handleChange} />
+                <input type="text" name="firstname" id="firstname" onChange={handleChange} value={account.firstname} />
 
                 <label htmlFor="lastname">Last Name</label>
-                <input type="text" name="lastname" id="lastname" onChange={handleChange} />
+                <input type="text" name="lastname" id="lastname" onChange={handleChange} value={account.lastname} />
 
                 <label htmlFor="acctType">Account Type</label>
-                <select name="acctType" id="acctType" required onChange={handleChange}>
+                <select name="acctType" id="acctType" required onChange={handleChange} value={account.acctType}>
                     <option value=""></option>
                     <option value="Saving">Saving</option>
                     <option value="Current">Current</option>
                 </select>
 
                 <label htmlFor="balance">Balance</label>
-                <input type="number" name="balance" id="balance" required onChange={handleChange} />
+                <input type="number" name="balance" id="balance" required onChange={handleChange} value={account.balance} />
 
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" onChange={handleChange} />
+                <input type="email" name="email" id="email" onChange={handleChange} value={account.email} />
 
                 <label htmlFor="dateOpened">Date Opened</label>
-                <input type="date" name="dateOpened" id="dateOpened" onChange={handleChange} />
+                <input type="date" name="dateOpened" id="dateOpened" onChange={handleChange} value={account.dateOpened} />
 
                 <label htmlFor="gender">Gender</label>
-                <select name="gender" id="gender" required onChange={handleChange}>
+                <select name="gender" id="gender" required onChange={handleChange} value={account.gender}>
                     <option disabled selected>Select Gender</option>
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
                 </select>
 
                 <label htmlFor="profileImage">Profile Image URL</label>
-                <input type="url" name="profileImage" id="profileImage" onChange={handleChange} />
+                <input type="url" name="profileImage" id="profileImage" onChange={handleChange} value={account.profileImage} />
 
                 <button type="Submit">Submit</button>
             </form>
